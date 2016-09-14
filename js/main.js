e5e332dca14a4d546b1f44d87997a48f214accac
nora-yauricaza <nora-yauricaza>
2016-09-14 16:58:02 -0500

    traduccion en español

---
+++ js/main.js
@@ -0,0 +1,5 @@
+document.getElementById("form-signin-heading").innerHTML="Por favor inicia sesión";
+document.getElementById("inputEmail").placeholder="Correo Electrónico";
+document.getElementById("inputPassword").placeholder="Contraseña";
+document.getElementsByTagName("span")[0].innerHTML="Recordar Datos";
+document.getElementsByTagName("button")[0].innerHTML="Iniciar sesión";
